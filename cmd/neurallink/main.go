823656f7a085b16e9ea6aa7aa259561249c1c5e0
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 20:51:48 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/neurallink/main.go
@@ -0,0 +1,23 @@
+// cmd/neurallink/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"neurallink/internal/neurallink"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := neurallink.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
